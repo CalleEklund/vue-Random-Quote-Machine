d7301a21bd2bf4d428c9622160b31a80d9e8f3e7
CalleEklund <eklund.calle@gmail.com>
2020-12-15 02:55:57 +0100

    implent api fetch

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="wrapper">
+    <Quote qoute="Test Qoute" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quote from "./components/Quote.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Quote,
+  },
+};
 </script>
 
 <style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+}
+#wrapper {
+  width: 100%;
+  height: 100vh;
+ 
+}
 #app {
+  background-color: rebeccapurple;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="wrapper">
+    <div id="quote-box" :style="state.styleBg">
+      <div id="text"></div>
+      <div id="author"></div>
+      <button id="new-quote" @click="changeBgStyle">New Quote</button>
+    </div>
+    <div id="footer">by Calle Eklund</div>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "Quote",
+  props: {
+    qoute: String,
+  },
+  setup() {
+    const state = reactive({
+      styleBg: {
+        backgroundColor: "rgb(248, 165, 194)",
+      },
+    });
+
+    function changeBgStyle() {
+      this.state.styleBg = {
+        backgroundColor: "#" + ((Math.random() * 0xffffff) << 0).toString(16),
+      };
+    }
+    return {
+      state,
+      changeBgStyle,
+    };
+  },
+};
+</script>
+
+<style scoped>
+body,
+button {
+  -webkit-transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
+}
+#quote-box {
+  width: 500px;
+  height: 300px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border: 5px solid #ffff00;
+  padding: 20px;
+}
+</style>
