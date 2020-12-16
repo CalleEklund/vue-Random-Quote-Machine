aad48e0269e406516d5c058db5d201970d2b8000
CalleEklund <eklund.calle@gmail.com>
2020-12-16 17:08:46 +0100

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <div id="wrapper">
-    <Quote qoute="Test Qoute" />
-  </div>
+  <Quote qoute="Test Qoute" />
 </template>
 
 <script>
+import { reactive } from "vue";
 import Quote from "./components/Quote.vue";
 
 export default {
@@ -12,25 +11,43 @@ export default {
   components: {
     Quote,
   },
+  setup() {
+    const state = reactive({
+      appStyle: {
+        backgroundColor: "rgb(102, 51, 153)",
+      },
+    });
+
+    return {
+      state,
+    };
+  },
 };
 </script>
 
 <style>
-*{
+* {
   box-sizing: border-box;
   margin: 0;
+  font-family: "Raleway", sans-serif;
 }
-#wrapper {
+#app-wrapper {
   width: 100%;
   height: 100vh;
- 
+  background-color: rgb(102, 51, 153);
 }
 #app {
-  background-color: rebeccapurple;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  -webkit-transition: all 1s ease-in-out;
+  transition: all 1s ease-in-out;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  width: 100vw;
+  height: 100vh;
 }
 </style>
--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="wrapper">
-    <div id="quote-box" :style="state.styleBg">
-      <div id="text"></div>
-      <div id="author"></div>
-      <button id="new-quote" @click="changeBgStyle">New Quote</button>
+    <div id="quote-box">
+      <div id="text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sapien orci,
+        placerat vitae eleifend at, gravida sit amet augue. Aliquam erat volutpat. Fusce
+        condimentum scelerisque justo, eu blandit lacus egestas ut.
+      </div>
+      <div id="author">- Lorem Ipsum</div>
     </div>
+    <button id="new-quote" :style="state.styleBg" @click="changeBgStyle">
+      New Quote
+    </button>
     <div id="footer">by Calle Eklund</div>
   </div>
 </template>
@@ -15,17 +21,20 @@ export default {
   name: "Quote",
   props: {
     qoute: String,
+    backgroundColor: String,
   },
   setup() {
     const state = reactive({
       styleBg: {
         backgroundColor: "rgb(248, 165, 194)",
+        color: "white",
       },
     });
 
     function changeBgStyle() {
+      const colorBg = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
       this.state.styleBg = {
-        backgroundColor: "#" + ((Math.random() * 0xffffff) << 0).toString(16),
+        backgroundColor: colorBg,
       };
     }
     return {
@@ -37,19 +46,46 @@ export default {
 </script>
 
 <style scoped>
-body,
-button {
-  -webkit-transition: all 0.4s ease-in-out;
-  transition: all 0.4s ease-in-out;
+#wrapper {
+  border: 1px solid #2d2d2d;
+  grid-area: 2/ 2 / span 3 / span 3;
+  display: inline-grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  border-radius: 3px;
 }
 #quote-box {
-  width: 500px;
-  height: 300px;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  border: 5px solid #ffff00;
-  padding: 20px;
+  grid-area: 1/2 / span 3 / span 3;
+  justify-self: stretch;
+  align-self: center;
+  font-size: 22px;
+}
+#text {
+  padding-bottom: 5rem;
+}
+#new-quote {
+  grid-row: 4;
+  grid-column: 5;
+  height: 5rem;
+  width: 8rem;
+  border: none;
+  justify-self: center;
+  align-self: center;
+  border-radius: 5px;
+  /**background-color: #d6c8c8;**/
+  transition: 0.4s ease-out;
+}
+#new-quote:hover {
+  cursor: pointer;
+  /**background-color: rgb(78, 78, 158);**/
+  transition: 0.4s ease-in;
+  color: white;
+  transform: scale(1.1, 1.1);
+}
+#footer {
+  grid-area: 5/2 / span 3 / span 3;
+  justify-self: center;
+  align-self: end;
+  padding-bottom: 15px;
 }
 </style>
